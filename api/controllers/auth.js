f122d454b2a78b89d993dda17b4a72b68f810941
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-01-31 22:12:21 +0800

    token is added

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,22 +1,50 @@
-import User from "../models/Users.js"
-import bycrypt from "bcrypt"
+import User from "../models/Users.js";
+import bcrypt from "bcrypt";
+import createError from "http-errors";
+import jwt from "jsonwebtoken";
 
-export const register = async (req, res, next)=>{
-    try{
-     const salt = bycrypt.genSaltSync(10);
-     const hash = bycrypt.hashSync(req.body.password, salt);
-        const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:hash,
-            
-        })
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
 
-    await newUser.save()
-    res.status(200).send("User has been created")
+    await newUser.save();
+    res.status(200).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
+};
 
-    }catch(err){
-        next(err)
-    }
-}
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password and username!"));
+
+    const token = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+       process.env.JWT
+      );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+      .cookie("access_token", token,
+      {httpOnly: true,})
+      .status(200)
+      .json({ ...otherDetails, token });
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,24 @@
 import express from "express";
+import { getUsers, updateUser, deleteUser, getUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// Protected route to check user authentication
+router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+    res.send("User, you are logged in");
+});
 
+// UPDATE
+router.put("/:id", updateUser);
 
-export default router
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getUsers);
+
+export default router;
